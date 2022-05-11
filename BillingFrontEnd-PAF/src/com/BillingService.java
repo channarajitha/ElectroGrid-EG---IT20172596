36e318e76373da00867262b154c307e29edcd571
channa <channa@192.168.17.1>
2022-05-12 00:21:24 +0530

    import java.util,javax.servlet and create webServlet in BillingService.java

--- BillingFrontEnd-PAF/src/com/BillingService.java
+++ BillingFrontEnd-PAF/src/com/BillingService.java
@@ -1,5 +1,40 @@
 package com;
 
-public class BillingService {
+
+import com.Billing;
+import java.io.IOException; 
+import java.util.HashMap; 
+import java.util.Map; 
+import java.util.Scanner;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/BillingService")
+public class BillingService extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	Billing billingObj = new Billing();
+
+    public BillingService() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String output = billingObj.insertBilling(request.getParameter("bAcc"),      
+				request.getParameter("bName"),
+				request.getParameter("bUsage"),
+				request.getParameter("bAmount")); 
+				response.getWriter().write(output);
+	}
 
 }
