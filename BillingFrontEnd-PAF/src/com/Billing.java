210e2668bc2ef2154b6269344ce66bfc2a6cd25e
channa <channa@192.168.17.1>
2022-05-11 23:13:27 +0530

    impoert java.sql and create db connection

--- BillingFrontEnd-PAF/src/com/Billing.java
+++ BillingFrontEnd-PAF/src/com/Billing.java
@@ -1,5 +1,63 @@
 package com;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
 public class Billing {
+	
+	private Connection connect() {
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			// Provide the correct details: DBServer/DBName, username, password
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/egsystem?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+
+	public String insertBilling(String bAcc, String bName, String bUsage, String bAmount)  
+	{   
+		String output = ""; 	 
+		try   
+		{    
+			Connection con = connect(); 
+	 
+			if (con == null)    
+			{return "Error while connecting to the database for inserting."; } 
+	 
+			// create a prepared statement 
+			String query = " insert into billing(`bID`,`bAcc`,`bName`,`bUsage`,`bAmount`)" + " values (?, ?, ?, ?, ?)"; 
+			PreparedStatement preparedStmt = con.prepareStatement(query); 
+	 
+			// binding values    
+			 preparedStmt.setInt(1, 0);
+			 preparedStmt.setString(2, bAcc);
+			 preparedStmt.setString(3, bName);
+			 preparedStmt.setString(4, bUsage);
+			 preparedStmt.setString(5, bAmount);
+			
+			// execute the statement    
+			preparedStmt.execute();    
+			con.close(); 
+	   
+			String newBilling = readBilling(); 
+			output =  "{\"status\":\"success\", \"data\": \"" + newBilling + "\"}";    
+		}   
+		catch (Exception e)   
+		{    
+			output =  "{\"status\":\"error\", \"data\": \"Error while inserting the billing.\"}";  
+			System.err.println(e.getMessage());   
+		} 		
+	  return output;  
+	} 	
 
 }
